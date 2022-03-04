503ad6823d5ac9790eccb148ea15da048a2a018a
ali065119057 <83912779+ali065119057@users.noreply.github.com>
2022-03-04 13:13:40 +0700

    Update dart_sample1.dart
    Add a read-only variable

--- dart_sample1.dart
+++ dart_sample1.dart
@@ -1,14 +1,25 @@
 //Define Class
 class Bicycle {
   int cadence; //variabel
-  int speed;
+  int _speed = 0;
+  int get speed => _speed;
   int gear;
-  Bicycle(this.cadence, this.speed, this.gear); //constructor
+  
+  Bicycle(this.cadence, this.gear); //constructor
+  
+    void applyBrake(int decrement) {
+    _speed += decrement;
+   }
+
+  void speedUp(int increment) {
+    _speed -= increment;
+   }
+ 
   @override
-  String toString() =>'Bicycle: $speed mph';  
-}
+  String toString() =>'Bicycle: $speed mph';
+  }
 
 void main() {
-  var bike= Bicycle(2, 0, 1); //Instantiate class
+  var bike= Bicycle(2, 1); //Instantiate class
   print (bike);
 }
