3c1fcf378d126d70293b403f1842a4964df90dae
ali065119057 <83912779+ali065119057@users.noreply.github.com>
2022-03-07 14:15:50 +0700

    Create Exercise 4.dart
    Implement an interface

---
+++ Exercise 4.dart
@@ -0,0 +1,50 @@
+import 'dart:math';
+
+/* 
+  Shape shapeFactory(String type){
+  if (type == 'circle') return Circle(2);
+  if (type == 'square') return Square(2);
+  throw 'Can\'t create $type.';
+}
+*/
+
+abstract class Shape{
+  factory Shape(String type){
+    if (type == 'circle') return Circle(2);
+    if (type == 'square') return Square(2);
+    throw 'Can\'t create $type.';
+  }
+  num get area;
+}
+
+class Circle implements Shape {
+  final num radius;
+  Circle(this.radius);
+  @override
+  num get area => pi * pow(radius, 2);
+}
+
+class Square implements Shape {
+  final num side;
+  Square(this.side);
+  @override
+  num get area => pow(side, 2);
+}
+
+class CircleMock implements Circle{
+  @override
+  num area = 0;
+  @override
+  num radius = 0;
+}
+
+main(){
+  //final circle = Circle(2);
+  //final square = Square(2);
+  //final circle = shapeFactory('circle');
+  //final square = shapeFactory('square');
+  final circle = Shape('circle');
+  final square = Shape('square');
+  print (circle.area);
+  print (square.area);
+}
